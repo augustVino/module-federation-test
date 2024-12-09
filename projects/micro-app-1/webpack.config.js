b94ca5ca983ec26a304c0af061cb74e0236a7940
Vino <zhangyang1@liepin.com>
2024-12-09 14:59:17 +0800

    feat: Enhance webpack config for module CSS and federation

--- projects/micro-app-1/webpack.config.js
+++ projects/micro-app-1/webpack.config.js
@@ -7,6 +7,8 @@ const { ModuleFederationPlugin } = require('@module-federation/enhanced/webpack'
 const path = require('path');
 const crypto = require('crypto');
 
+const isPrd = false;
+
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
@@ -30,33 +32,74 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(less|css)$/,
-        exclude: /node_modules/,
+        // key: 'moduleLess',
+        test: /\.module\.less$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          isPrd
+            ? {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  esModule: true
+                }
+              }
+            : 'style-loader',
           {
             loader: 'css-loader',
             options: {
-              esModule: false,
-              importLoaders: 1,
               modules: {
-                auto: (resourcePath) => resourcePath.endsWith('.module.less'),
-                // localIdentName: '[local]__[hash:8]',
-                getLocalIdent: (context, localIdentName, localName, options) => {
-                  // 返回一个包含文件路径和名称的更具体的哈希
-                  //   return `${localName}-${context.resourcePath}`;
-                  const hash = crypto.createHash('sha1');
-                  hash.update(context.resourcePath + localName);
-                  return `${localName}-${hash.digest('hex').slice(0, 8)}`;
-                }
-              }
+                auto: true,
+                // getLocalIdent: (context, localIdentName, localName, options) => {
+                //   // 返回一个包含文件路径和名称的更具体的哈希
+                //   //   return `${localName}-${context.resourcePath}`;
+                //   const hash = crypto.createHash('sha1');
+                //   hash.update(context.resourcePath + localName);
+                //   return `${localName}-${hash.digest('hex').slice(0, 8)}`;
+                // },
+                namedExport: false,
+                /**
+                 * 值取决与namedExport，namedExport为 true 默认 as-is，false 默认 camel-case-only
+                 * as-is: 类名将按原样导出
+                 * camel-case-only: 类名将使用驼峰命名（类名会被改写）
+                 */
+                exportLocalsConvention: 'as-is',
+                localIdentName: !isPrd
+                  ? '[path][name]__[local]--[hash:base64:5]'
+                  : '[hash:base64:5]'
+              },
+              sourceMap: !isPrd,
+              importLoaders: 2
             }
           },
           {
             loader: 'less-loader',
             options: {
               lessOptions: {
-                sourceMap: true,
+                math: 'always',
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        // key: 'css&less',
+        test: /\.(less|css)$/,
+        exclude: [/node_modules/, /\.module.less$/],
+        use: [
+          isPrd
+            ? {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  esModule: true
+                }
+              }
+            : 'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                math: 'always',
                 javascriptEnabled: true
               }
             }
